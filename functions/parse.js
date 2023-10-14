4802858e4188fdc4612ecf9717c32a7d44e69879
NewGrafon <ultrafig73@gmail.com>
2023-10-15 00:03:03 +0400

    new func

--- functions/parse.js
+++
@@ -1,9 +0,0 @@
-export function parseNumberOrNull(value) {
-    if (value === undefined || value.trim().length === 0) {
-        return null;
-    } else {
-        return Number(value.toString());
-    }
-}
-
-module.exports.parseNumberOrNull = parseNumberOrNull;
