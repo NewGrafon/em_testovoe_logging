0e375fcafb6f654497eb7cf7d7bdaeded8d82843
NewGrafon <ultrafig73@gmail.com>
2023-10-15 19:50:00 +0400

    remove unused packages

--- src/rabbitmq/recieve.js
+++ src/rabbitmq/recieve.js
@@ -1,7 +1,6 @@
 const amqp = require('amqplib/callback_api');
-const fetch = require('node-fetch');
+const {selectHandledToMessage} = require("../users-history/users-history.service");
 const RMQ_URL = process.env.RABBITMQ_URL;
-const LOGGING_SERVICE_URL = process.env.LOGGING_SERVICE_URL;
 
 function RMQ_ReceiveMessage(queue) {
     amqp.connect(RMQ_URL, (error0, connection) => {
@@ -28,14 +27,7 @@ function RMQ_ReceiveMessage(queue) {
                     const obj = JSON.parse(msg.content.toString());
                     console.log(' [x] Received "%s"', obj);
 
-                    const sendObj = await fetch(`${LOGGING_SERVICE_URL}/users`, {
-                        method: 'POST',
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify(obj)
-                    });
-                    console.log(' [x] Fetch result: "%s"', await sendObj.json());
+                    console.log(' [x] Log result: "%s"', await selectHandledToMessage(obj));
                 },
                 {
                     noAck: true,
